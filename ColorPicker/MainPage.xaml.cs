14d0c7b2de1561995ebd7f017b2697404b2b0692
Lucas Meireles <meireles31@msn.com>
2018-01-29 00:39:18 +0000

    ColorPicker MVP

--- ColorPicker/MainPage.xaml.cs
+++ ColorPicker/MainPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +13,37 @@ namespace ColorPicker
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Color currentColor;
+
         public MainPage()
         {
             this.InitializeComponent();
+            WatchCursor();
+        }
+
+        public async void WatchCursor()
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(10));
+                UpdatePixelValues();
+                UpdateLabels();
+            }
+        }
+
+        private void UpdateLabels()
+        {
+            this.RedValueLabel.Text = currentColor.R.ToString();
+            this.GreenValueLabel.Text = currentColor.G.ToString();
+            this.BlueValueLabel.Text = currentColor.B.ToString();
+            this.CurrentPixelRectangle.Fill = new SolidColorBrush(currentColor);
+        }
+
+        private void UpdatePixelValues()
+        {
+            var pointerPosition = Windows.UI.Core.CoreWindow.GetForCurrentThread().PointerPosition;
+            var color = PixelHelper.GetColorAt((int)pointerPosition.X, (int)pointerPosition.Y);
+            currentColor = color;
         }
     }
 }
---
+++ ColorPicker/PixelHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace ColorPicker
+{
+    static class PixelHelper
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            var pixel = GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (byte)((pixel >> 0) & 0xff), (byte)((pixel >> 8) & 0xff), (byte)((pixel >> 16) & 0xff));
+        }
+    }
+}
